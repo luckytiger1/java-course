bfc24677514242f142b37b44fc157be7fa63f638
Daniyar Ketebay <daniyar.abc@gmail.com>
2021-01-06 02:09:57 +0600

    refactor: remove unused imports

--- lesson5/src/main/java/com/geekbrains/lesson5/MainApp.java
+++ lesson5/src/main/java/com/geekbrains/lesson5/MainApp.java
@@ -1,9 +1,7 @@
 package com.geekbrains.lesson5;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
 public class MainApp {
     public static void main(String[] args) {
